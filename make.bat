a0ed25b61ed1ef85d1eaa7f26ffb4da83a58691a
Matteo Mattei <matteo.mattei@gmail.com>
2015-12-04 14:02:49 +0100

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,9 +1,7 @@
 del /Q Output
 del /Q dist
 del /Q build
-del /Q MSVCP90.DLL
 
-copy C:\Windows\System32\MSVCP90.DLL .
 C:\python27\python.exe setup.py py2exe
 "C:\Program Files (x86)\Inno Setup 5\iscc.exe" setup.iss
 pause
